19517a3921c45cbde3890911ad5e2c8ecfd5dbaa
Keniel Calixton <kenielcalixton@hotmail.com>
2024-07-14 18:56:06 -0300

    Desafio criado menu
    Estudo das aptidão inserido menu

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,54 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Scanner scanner = new Scanner(System.in);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
+		System.out.print("Digite o nome do cliente: ");
+		String nomeCliente = scanner.nextLine();
+
+		Cliente cliente = new Cliente();
+		cliente.setNome(nomeCliente);
+
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+
+		System.out.println("Menu:");
+		System.out.println("1. Depositar");
+		System.out.println("2. Sacar");
+		System.out.println("3. Transferir");
+		System.out.println("4. Imprimir Extrato");
+		System.out.print("Digite a opção: ");
+		int opcao = scanner.nextInt();
+
+		switch (opcao) {
+			case 1:
+				System.out.print("Digite o valor do depósito: ");
+				double valorDeposito = scanner.nextDouble();
+				cc.depositar(valorDeposito);
+				cc.imprimirExtrato();
+				break;
+			case 2:
+				System.out.print("Digite o valor do saque: ");
+				double valorSaque = scanner.nextDouble();
+				cc.sacar(valorSaque);
+				cc.imprimirExtrato();
+				break;
+			case 3:
+				System.out.print("Digite o valor da transferência: ");
+				double valorTransferencia = scanner.nextDouble();
+				cc.transferir(valorTransferencia, poupanca);
+				cc.imprimirExtrato();
+				poupanca.imprimirExtrato();
+				break;
+			case 4:
+				cc.imprimirExtrato();
+				poupanca.imprimirExtrato();
+				break;
+			default:
+				System.out.println("Opção inválida.");
+		}
 	}
 
 }
